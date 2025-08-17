9dd34bca1817fa5351b7e2d905af47e42f39e2be
kimjongappreciator <nonexistentexistentialist1@gmail.com>
2025-08-16 20:03:02 -0500

    a

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,33 @@
 <script setup lang="ts">
-import { useMonthStore } from '@/models/monthsStore';
-import MonthCard from './MonthCard.vue';
+import { useMonthStore } from "@/models/monthsStore";
+import MonthCard from "./MonthCard.vue";
+import Carousel from "./ui/carousel/Carousel.vue";
+import CarouselContent from "./ui/carousel/CarouselContent.vue";
+import CarouselItem from "./ui/carousel/CarouselItem.vue";
+import CarouselPrevious from "./ui/carousel/CarouselPrevious.vue";
+import CarouselNext from "./ui/carousel/CarouselNext.vue";
 
-
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 const monthsStore = useMonthStore();
-
-
 </script>
 
 <template>
-  <h1 class="text-center"> Bienvenido {{ msg }}</h1>
+  <h1 class="text-center">Bienvenido {{ msg }}</h1>
   <div class="flex flex-wrap justify-center gap-4">
+    <Carousel>
+      <CarouselContent class="w-150">
+        <CarouselItem v-for="(month, index) in monthsStore.months" :key="index">
+          <MonthCard :month="month"></MonthCard>
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+    <!--
     <div v-for="(month, index) in monthsStore.months" :key="index" class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 m-2">
       <MonthCard :month="month"></MonthCard>
-    </div>
+    </div>-->
   </div>
-
 </template>
 
 <style scoped></style>
